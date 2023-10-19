a550e1ec3bea2284ad237dc0dddd06ae40b15489
bhavya0210 <bhavyaroli@gmail.com>
2023-10-20 00:36:07 +0530

    driver homepage

--- applications/DriverController.py
+++ applications/DriverController.py
@@ -6,3 +6,10 @@ from sqlalchemy.orm import Session, relationship
 from applications.database import Base, db, engine
 from applications.config import LocalDevelopementConfig
 
+@app.route("/driver/1/home", methods=["GET", "POST"])
+def homepage():
+    if(request.method=='GET'):
+        #temp:
+        driverInfo = {"did":1, "name":"abc"}
+        return render_template('driverHomePage.html', driverInfo = driverInfo)
+    
