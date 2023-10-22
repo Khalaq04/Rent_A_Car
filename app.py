5af26c96a26e6f942885ac58f9856cf79aa17b42
KHALAQ <khalaq04@khalaq-ubuntu-20.04>
2023-10-22 23:38:37 +0530

    Removed sqlalchemy

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask
-from applications import config
-from applications.config import LocalDevelopementConfig
-from applications.database import db
 
 app = None
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
-    app.config.from_object(LocalDevelopementConfig)
-    db.init_app(app)
     app.app_context().push()
     return app
 
--- applications/DriverController.py
+++ applications/DriverController.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask import current_app as app
-import sqlalchemy
-from sqlalchemy import create_engine, Table, Column, Integer, String, ForeignKey, select
-from sqlalchemy.orm import Session, relationship
-from applications.database import Base, db, engine
-from applications.config import LocalDevelopementConfig
 import psycopg2
 
 def get_car_details():
     conn = psycopg2.connect(
         host="localhost",
-        database="bhavya",
-        user="bhavya",
-        password="pintoo.9"
+        database="crd",
+        user="crd",
+        password="crd"
     )
     cursor = conn.cursor()
     query="select * from cars"
